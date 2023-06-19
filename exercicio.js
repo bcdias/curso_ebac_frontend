27301746141763d2cc30a280ba456bccf0da589d
Bruno Dias <dias.desenvolvedor@gmail.com>
2023-06-19 13:19:03 -0300

    Exercício do módulo de ts

---
+++ exercicio.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicaNumeros(primeiroNumero, segundoNumero) {
+    const produto = primeiroNumero * segundoNumero;
+    return produto;
+}
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicaNumeros(2, 110));
+console.log(dizOla('Mariana'));
---
+++ exercicio.ts
@@ -0,0 +1,12 @@
+function multiplicaNumeros(primeiroNumero: number, segundoNumero: number): number {
+    const produto = primeiroNumero * segundoNumero;
+    return produto
+} 
+
+function dizOla(nome: string): string {
+    return `Olá ${nome}`
+}
+
+console.log(multiplicaNumeros(2,110));
+
+console.log(dizOla('Mariana'))
